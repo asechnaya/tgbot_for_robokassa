1b5148b30221e300a7b8d3162308e357f720983e
Asja <anutya@mail.ru>
2019-06-18 15:56:50 +0300

    Update robotelebot.py

--- robotelebot.py
+++ robotelebot.py
@@ -1,58 +1,61 @@
 # -- coding: utf-8 -*-
-
 import logging
-
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler
 
 import datetime
 import scoring
 from operations import OperationSts
+from support import SkpSupport
 from parametres import Robowebsites
 from payment_status import payment_state
+from config import botpath as botpath
+
+chatik = '-100 номер чатика'
+BOTTOKEN = '%bot token'
+TIMER = 43200 #12 часов
+tr_kn = '-100 номер канала'
 
 # Логгирование
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
-                    filename='bot.log'
+                    filename=botpath
                     )
-
-#-------------------------------------------
-operations_p = scoring.operations
-operations = OperationSts(operations_p)
-operation = operations.info()
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-SkpSupport_p = SkpSupport(scoring.ddd)
-SkpSupport = SkpSupport_p.info()
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-botsstate_all = scoring.botsstate_all
-botstate_text = scoring.bot_state_text
-allbotstate_text = scoring.all_bot_state_text
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mixplat_text = payment_state(*scoring.mixplat_text)
-mobilewallet_text = payment_state(*scoring.mobilewallet_text)
-alfabank_text = payment_state(*scoring.alfabank_text)
-PaySendBank_text = payment_state(*scoring.PaySendBank_text)
-OceanBank_text = payment_state(*scoring.OceanBank_text)
-QiwiBank_text = payment_state(*scoring.QiwiBank_text)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-status = Robowebsites()
-webstatus = status.info()
+RabbitMq_text = scoring.RabbitMq_text()
 #-------------------------------------------
 
 
-RabbitMq_text = scoring.RabbitMq_text
+def inv_check_param():
+    return 'Cписок контрольных параметров системы: на {}: \n\n{}8. {}\n9. RabbitMq {}{}\n' \
+           '10. Состояние роботов {}'.format(datetime.datetime.now(),
+                                             OperationSts(scoring.operations_text()).info(),
+                                             inv_PsStates_info(),
+                                             *scoring.RabbitMq_text()[0:len(RabbitMq_text)],
+                                             scoring.bot_state_text())
 
-PsStates_info = 'Состояние платежных систем:\n  {} \n   {} \n   {} \n   {} \n   {}\n     {}'.format(mixplat_text, mobilewallet_text, alfabank_text, PaySendBank_text, OceanBank_text, QiwiBank_text)
 
-check_param = 'Cписок контрольных параметров системы: на {}: \n\n{}8. {}\n9. RabbitMq {}{}\n10. Состояние роботов {}'.format(datetime.datetime.now(), operation, PsStates_info, *RabbitMq_text[0:len(RabbitMq_text)], botstate_text)
+def inv_PsStates_info():
+    return 'Состояние платежных систем:\n  {} \n   {} \n   {} \n   {} \n   {}\n     {}'.format(
+                                                                                                     payment_state(*scoring.mixplat_text()),
+                                                                                                     payment_state(*scoring.mobilewallet_text()),
+                                                                                                     payment_state(*scoring.alfabank_text()),
+                                                                                                     payment_state(*scoring.PaySendBank_text()),
+                                                                                                     payment_state(*scoring.OceanBank_text()),
+                                                                                                     payment_state(*scoring.QiwiBank_text())
+                                                                                                )
+def inv_webstatus():
+    return 'Проверка сайтов: на {}: \n \n{}'.format(datetime.datetime.now(), Robowebsites().info())
 
-webstatus = 'Проверка сайтов: на {}: \n \n{}'.format(datetime.datetime.now(), webstatus)
+def inv_about_bot():
+    return 'Состояние ботов:\n{}'.format(scoring.bot_state_text())
 
-about_bot = 'Состояние ботов:\n{}'.format(allbotstate_text)
+def inv_all_about_bot():
+    return 'Состояние ботов:\n{}'.format(scoring.all_bot_state_text())
 
-SkpSupport_info = 'Для клиентской поодержки:\n{}'.format(SkpSupport)
+def SkpSupport_info():
+    return 'Для клиентской поодержки {}:\n{}'.format(datetime.datetime.now(), SkpSupport(scoring.inv_skp()).info())
+
+
 
 
 
@@ -67,73 +70,69 @@ start_text ='У нас есть:\n /start - список того, что ты �
             '@RoboPing - робот от Вадима, который уведомляет, если что-то отвалилось\n' \
             '/SkpSupport - че-т там для СКП'
 
-
-chatik = '-1001102275465' # '-1001221778947' - мой чат, # '-1001102275465' - STS,
-
 def start(bot, update):
     update.message.reply_text(start_text)
 
 # присылает состояние сайтов и операций
 def callback_timer(bot, update):
-    bot.send_message(chat_id=chatik,
-                     text=check_param)
-    bot.send_message(chat_id=chatik,
-                     text=webstatus)
-
-# просто пишет сообщения о ботах, операциях и сайтах
-
-def warning_web(bot, update):
-    update.message.reply_text(webstatus)
-
-def warning_operation(bot, update):
-    update.message.reply_text(operation)
-
-def warning_bot(bot, update):
-    update.message.reply_text(about_bot)
-
-def warning_PsStates_info(bot, update):
-    update.message.reply_text(PsStates_info)
-
-def warning_SkpSupport_info(bot, update):
-    update.message.reply_text(SkpSupport_info)
-
-def warning_Support_info(bot, update):
-    bot.send_message(chat_id=chatik,  # '-1001102275465' - STS,
-                     text=SkpSupport_info)
-
+    bot.send_message(chat_id=tr_kn,
+                     text=inv_check_param())
+    bot.send_message(chat_id=tr_kn,
+                     text=inv_webstatus())
 
 
 #-- все вместе
 
 def sys_check_param(bot, update):
-    update.message.reply_text(check_param)
+    update.message.reply_text(inv_check_param())
 
 def allstat_sts(bot, update):
-    bot.send_message(chat_id=chatik,  # '-1001102275465' - STS,
-                     text=check_param)
+    bot.send_message(chat_id=chatik,  
+                     text=inv_check_param())
 
 # рассказывает о ботах
 def botsstate(bot, update):
-    bot.send_message(chat_id=chatik,  # '-1001102275465' - STS,
-                     text=about_bot)
+    bot.send_message(chat_id=chatik,  
+                     text=inv_all_about_bot())
 
+    
+# просто пишет сообщения о ботах, операциях и сайтах
 
+def warning_web(bot, update):
+    update.message.reply_text(inv_webstatus())
+
+def warning_operation(bot, update):
+    update.message.reply_text(OperationSts(scoring.operations_text()).info())
+
+def warning_bot(bot, update):
+    update.message.reply_text(inv_all_about_bot())
+
+def warning_PsStates_info(bot, update):
+    update.message.reply_text(inv_PsStates_info())
+
+def warning_SkpSupport_info(bot, update):
+    update.message.reply_text(SkpSupport_info())
+
+def warning_Support_info(bot, update):
+    bot.send_message(chat_id=chatik, 
+                     text=SkpSupport_info)
 
 def main():
-    updater = Updater("821731132:AAFQEQOBsequ3ljKlG_6KU_uv37hogODT_M")#, request_kwargs=REQUEST_KWARGS)
+
+    updater = Updater(BOTTOKEN)
     dp = updater.dispatcher  # принимает входящие сообщения и посылает их куда-то
     #инструкция
     dp.add_handler(CommandHandler("start", start))
 
-    # Запускаем так, чтобы каждые 6 часов робот писал в чатик
-    #updater.job_queue.run_repeating(callback_timer, interval=28800, first=0)
-
-
+    # Запускаем так, чтобы каждые 12 часов робот писал в чатик
+    updater.job_queue.run_repeating(callback_timer, interval=TIMER, first=0)
 
     #Команды для общего чатика
     dp.add_handler(CommandHandler("stsbotsstate", botsstate))
     dp.add_handler(CommandHandler("warning", callback_timer))
+    dp.add_handler(CommandHandler("allstat_sts", allstat_sts))
     #-------------------------
+    #Команды для индвидуальной обработки
     dp.add_handler(CommandHandler("webstatus", warning_web))
     dp.add_handler(CommandHandler("operation", warning_operation))
     dp.add_handler(CommandHandler("warning_bot", warning_bot))
@@ -141,14 +140,10 @@ def main():
     dp.add_handler(CommandHandler("SkpSupport", warning_SkpSupport_info))
     dp.add_handler(CommandHandler("Support", warning_Support_info))
     dp.add_handler(CommandHandler("sys_check_param", sys_check_param))
-    dp.add_handler(CommandHandler("allstat_sts", allstat_sts))
-
 
     # Handler -- обработчик
     updater.start_polling()  # отправь эти данные платформе телеграм
     updater.idle()  # Жди, пока тебе телеграм что-то пришлет
-
-
 
 
 if __name__ == '__main__':
