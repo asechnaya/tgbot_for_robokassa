a4d572322e6b2a3a09049506dcb037e3427f184f
Asja <anutya@mail.ru>
2019-06-18 15:58:35 +0300

    Update parametres.py

--- parametres.py
+++ parametres.py
@@ -3,23 +3,24 @@ import requests
 class Robowebsites(object):
 
     def __init__(self):
-        self.color = 'blue'
-        robomarket = requests.get('http://robo.market').status_code
-        opg_robokassa = requests.get('http://opg.robokassa.ru').status_code
-        partner_robokassa = requests.get('http://partner.robokassa.ru').status_code
-        my_robokassa = requests.get('https://auth.robokassa.ru/my/Account/Login', auth=('amakarovskaya@robokassa.ru', 'Anitarat1')).status_code
+        self.update_data()
+
+    def update_data(self):
+        robomarket = requests.get('https://robo.market').status_code
+        opg_robokassa = requests.get('https://opg.robokassa.ru').status_code
+        partner_robokassa = requests.get('https://partner.robokassa.ru').status_code
+        my_robokassa = requests.get('https://my.robokassa.ru').status_code
         auth_robokassa = requests.get('http://auth.robokassa.ru/my').status_code
         admin_roboxchange = requests.get('https://admin.roboxchange.com/admin2/').status_code
-        support_robokassa = requests.get('http://support.robokassa.ru/').status_code
+        support_robokassa = requests.get('https://support.robokassa.ru/').status_code
         caravanru = requests.get('https://www.caravan.ru/').status_code
         self.p = [robomarket, opg_robokassa, partner_robokassa, my_robokassa, auth_robokassa, admin_roboxchange,
-                   support_robokassa, caravanru]
+                  support_robokassa, caravanru]
         self.webname = ['robomarket', 'opg_robokassa', 'partner_robokassa', 'my_robokassa', 'auth_robokassa',
-                   'admin_roboxchange',
-                   'support_robokassa', 'caravanru']
-
+                        'admin_roboxchange',
+                        'support_robokassa', 'caravanru']
     def info(self):
-
+        self.update_data()
         counter = 0
         for i in range(len(self.p)):
             if self.p[i] == 200 or self.p[i] == 403:
@@ -36,7 +37,7 @@ class Robowebsites(object):
                     '3. partner.robokassa.ru: %s\n' \
                     '4. my.robokassa.ru:  %s\n' \
                     '5. auth.robokassa.ru:  %s\n' \
-                    '6. admin.roboxchange.ru: %s\n' \
+                    '6. admin.roboxchange.ru: : %s\n' \
                     '7. support.robokassa.ru: %s\n' \
                     '8. caravan.ru: %s' \
                     '%s' % (self.p[0], self.p[1], self.p[2], self.p[3], self.p[4], self.p[5], self.p[6], self.p[7], alarm)
