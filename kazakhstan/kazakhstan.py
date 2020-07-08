1f85acc94c5e1be92b6fa8c5d3ee5275ef691676
Asja Makarowska(ya) <anutya@mail.ru>
2020-07-08 16:36:43 +0300

    Update kazakhstan.py

--- kazakhstan/kazakhstan.py
+++ kazakhstan/kazakhstan.py
@@ -5,16 +5,20 @@ from config import cert
 
 
 link = 'https://admin.roboxchange.com/admin2/Operations/Search'
-
 headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
 
 
 def kazakhstan_operations_counter():
     with requests.Session() as s:
         entry_number = 0  # счетчик числа записей с номерами операций
+        opers = []
         response = s.post(link, data=payload(), cert=cert, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
+        # print(soup)
         for entry in soup.select("tr > td:nth-child(2) > a"):
-            print(entry.get_text(strip=True))
             entry_number += 1
-        return entry_number
+            opers.append(entry.get_text(strip=True))
+        return entry_number, opers
+
+
+print(kazakhstan_operations_counter())
